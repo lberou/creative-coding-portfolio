9f7ec11c61d8e322fd3a8ca2a905ecc1c8ade5ce
lberou <46044464+lberou@users.noreply.github.com>
2026-09-04 15:10:30 +0300

    Simplify canvas creation in setup function

--- 03-particle-playground/sketch.js
+++ 03-particle-playground/sketch.js
@@ -9,9 +9,7 @@ const FORCE = 0.05;
 const MAX_SPEED = 5;
 
 function setup() {
-  const canvas = createCanvas(500, 500);
-  canvas.parent("sketch-container");
-
+  createCanvas(500, 500);
   noStroke();
 
   for (let i = 0; i < PARTICLE_COUNT; i++) {
